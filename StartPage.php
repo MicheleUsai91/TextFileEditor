f01c5e60dfa63077a012ff50346aacb2c8eccd4d
Michele Usai <micheleusai@Micheles-MacBook-Air.local>
2026-04-28 19:11:55 +0200

    ITA

--- StartPage.php
+++ StartPage.php
@@ -5,7 +5,7 @@ include './main.php';
 ?>
 
 <!DOCTYPE html>
-<html lang="it">
+<html lang="it-IT">
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
